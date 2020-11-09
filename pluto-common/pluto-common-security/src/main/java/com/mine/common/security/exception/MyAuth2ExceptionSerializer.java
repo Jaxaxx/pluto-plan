0dc593a560cdf99963c26f572b0c59be26592736
liming <liming@wenxiangcn.com>
2020-11-10 00:24:59 +0800

    update common result

--- pluto-common/pluto-common-security/src/main/java/com/mine/common/security/exception/MyAuth2ExceptionSerializer.java
+++ pluto-common/pluto-common-security/src/main/java/com/mine/common/security/exception/MyAuth2ExceptionSerializer.java
@@ -3,6 +3,7 @@ package com.mine.common.security.exception;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.mine.common.core.constant.ResultCode;
 import lombok.SneakyThrows;
 
 /**
