f3e9ab2620f6e1302624067f1506c92d383b69ff
liming <liming@wenxiangcn.com>
2020-11-10 11:45:48 +0800

    add spring-boot-admin

---
+++ pluto-visual/pluto-monitor/src/main/java/com/mine/monitor/MonitorApplication.java
@@ -0,0 +1,19 @@
+package com.mine.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Jax-li
+ */
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
