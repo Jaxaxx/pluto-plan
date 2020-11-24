923dba72b19d2e0c31ef7b41d0c18bccb003215c
liming <liming@wenxiangcn.com>
2020-11-24 18:00:33 +0800

    update same file

--- pluto-common/pluto-common-security/src/main/java/org/springframework/security/oauth2/provider/token/DefaultUserAuthenticationConverter.java
+++ pluto-common/pluto-common-security/src/main/java/org/springframework/security/oauth2/provider/token/DefaultUserAuthenticationConverter.java
@@ -20,7 +20,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.util.StringUtils;
 
@@ -31,38 +30,37 @@ import java.util.Map;
 /**
  * Default implementation of {@link UserAuthenticationConverter}. Converts to and from an Authentication using only its
  * name and authorities.
- * 
+ *
  * @author Dave Syer
- * 
  */
 public class DefaultUserAuthenticationConverter implements UserAuthenticationConverter {
 
-	private Collection<? extends GrantedAuthority> defaultAuthorities;
+    private Collection<? extends GrantedAuthority> defaultAuthorities;
 
-	private UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
 
-	private static final String N_A = "N/A";
+    private static final String N_A = "N/A";
 
-	/**
-	 * Optional {@link UserDetailsService} to use when extracting an {@link Authentication} from the incoming map.
-	 * 
-	 * @param userDetailsService the userDetailsService to set
-	 */
-	public void setUserDetailsService(UserDetailsService userDetailsService) {
-		this.userDetailsService = userDetailsService;
-	}
+    /**
+     * Optional {@link UserDetailsService} to use when extracting an {@link Authentication} from the incoming map.
+     *
+     * @param userDetailsService the userDetailsService to set
+     */
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
-	/**
-	 * Default value for authorities if an Authentication is being created and the input has no data for authorities.
-	 * Note that unless this property is set, the default Authentication created by {@link #extractAuthentication(Map)}
-	 * will be unauthenticated.
-	 * 
-	 * @param defaultAuthorities the defaultAuthorities to set. Default null.
-	 */
-	public void setDefaultAuthorities(String[] defaultAuthorities) {
-		this.defaultAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList(StringUtils
-				.arrayToCommaDelimitedString(defaultAuthorities));
-	}
+    /**
+     * Default value for authorities if an Authentication is being created and the input has no data for authorities.
+     * Note that unless this property is set, the default Authentication created by {@link #extractAuthentication(Map)}
+     * will be unauthenticated.
+     *
+     * @param defaultAuthorities the defaultAuthorities to set. Default null.
+     */
+    public void setDefaultAuthorities(String[] defaultAuthorities) {
+        this.defaultAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList(StringUtils
+                .arrayToCommaDelimitedString(defaultAuthorities));
+    }
 
 //	public Map<String, ?> convertUserAuthentication(Authentication authentication) {
 //		Map<String, Object> response = new LinkedHashMap<String, Object>();
@@ -87,44 +85,49 @@ public class DefaultUserAuthenticationConverter implements UserAuthenticationCon
 //		return null;
 //	}
 
-	// 重写已使得userAuthentication.getPrincipal获取的是MyUser对象
-	@Override
-	public Map<String, ?> convertUserAuthentication(Authentication userAuthentication) {
-		Map<String, Object> authMap = new LinkedHashMap<>();
-		authMap.put(USERNAME, userAuthentication.getName());
-		if (userAuthentication.getAuthorities() != null && !userAuthentication.getAuthorities().isEmpty()) {
-			authMap.put(AUTHORITIES, AuthorityUtils.authorityListToSet(userAuthentication.getAuthorities()));
-		}
-		return authMap;
-	}
+    /**
+     * 重写已使得userAuthentication.getPrincipal获取的是MyUser对象
+     *
+     * @param userAuthentication userAuthentication
+     * @return map
+     */
+    @Override
+    public Map<String, ?> convertUserAuthentication(Authentication userAuthentication) {
+        Map<String, Object> authMap = new LinkedHashMap<>();
+        authMap.put(USERNAME, userAuthentication.getName());
+        if (userAuthentication.getAuthorities() != null && !userAuthentication.getAuthorities().isEmpty()) {
+            authMap.put(AUTHORITIES, AuthorityUtils.authorityListToSet(userAuthentication.getAuthorities()));
+        }
+        return authMap;
+    }
 
-	@Override
-	public Authentication extractAuthentication(Map<String, ?> map) {
-		if (map.containsKey(USERNAME)) {
-			Collection<? extends GrantedAuthority> authorities = getAuthorities(map);
+    @Override
+    public Authentication extractAuthentication(Map<String, ?> map) {
+        if (map.containsKey(USERNAME)) {
+            Collection<? extends GrantedAuthority> authorities = getAuthorities(map);
 
-			Long userId = Convert.toLong(map.get(SecurityConstants.DETAILS_USER_ID));
-			String username = (String) map.get(SecurityConstants.DETAILS_USERNAME);
-			String phone = (String) map.get(SecurityConstants.DETAILS_PHONE);
-			String name = (String) map.get(SecurityConstants.DETAILS_NAME);
-			MyUser user = new MyUser(userId, phone, username, N_A, name,true, true, true, true, authorities);
-			return new UsernamePasswordAuthenticationToken(user, N_A, authorities);
-		}
-		return null;
-	}
+            Long userId = Convert.toLong(map.get(SecurityConstants.DETAILS_USER_ID));
+            String username = (String) map.get(SecurityConstants.DETAILS_USERNAME);
+            String phone = (String) map.get(SecurityConstants.DETAILS_PHONE);
+            String name = (String) map.get(SecurityConstants.DETAILS_NAME);
+            MyUser user = new MyUser(userId, phone, username, N_A, name, true, true, true, true, authorities);
+            return new UsernamePasswordAuthenticationToken(user, N_A, authorities);
+        }
+        return null;
+    }
 
-	private Collection<? extends GrantedAuthority> getAuthorities(Map<String, ?> map) {
-		if (!map.containsKey(AUTHORITIES)) {
-			return defaultAuthorities;
-		}
-		Object authorities = map.get(AUTHORITIES);
-		if (authorities instanceof String) {
-			return AuthorityUtils.commaSeparatedStringToAuthorityList((String) authorities);
-		}
-		if (authorities instanceof Collection) {
-			return AuthorityUtils.commaSeparatedStringToAuthorityList(StringUtils
-					.collectionToCommaDelimitedString((Collection<?>) authorities));
-		}
-		throw new IllegalArgumentException("Authorities must be either a String or a Collection");
-	}
+    private Collection<? extends GrantedAuthority> getAuthorities(Map<String, ?> map) {
+        if (!map.containsKey(AUTHORITIES)) {
+            return defaultAuthorities;
+        }
+        Object authorities = map.get(AUTHORITIES);
+        if (authorities instanceof String) {
+            return AuthorityUtils.commaSeparatedStringToAuthorityList((String) authorities);
+        }
+        if (authorities instanceof Collection) {
+            return AuthorityUtils.commaSeparatedStringToAuthorityList(StringUtils
+                    .collectionToCommaDelimitedString((Collection<?>) authorities));
+        }
+        throw new IllegalArgumentException("Authorities must be either a String or a Collection");
+    }
 }
